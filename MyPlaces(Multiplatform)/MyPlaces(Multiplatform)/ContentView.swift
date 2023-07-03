af2263bd37b2404e387b76f5fb359d18442fc7f5
Hong Le <hongle622@gmail.com>
2023-07-03 12:13:49 -0700

    "added MyPlaces for multiplatform"

--- MyPlaces(Multiplatform)/MyPlaces(Multiplatform)/ContentView.swift
+++ MyPlaces(Multiplatform)/MyPlaces(Multiplatform)/ContentView.swift
@@ -9,15 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        VStack(alignment: .leading){
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            } //hstack
+        }//vstack
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+    } //body
+} //contentview
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
